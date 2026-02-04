26b09e68d9ffe8a9d0ee803ca93a2bbd8d63f56d
Dushyant-Bhardwaj <dushyantpandit005@gmail.com>
2026-02-04 13:15:48 +0530

    initial commit

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const app=express();
 app.get("/",(req,res)=>{
     res.send("this is my home page");
 });
+app.get("/about",(req,res)=>{
+    res.send("this is my about page");
+});
+app.get("/contact",(req,res)=>{
+    res.send("this is my contact page");
+});
+app.get("/help",(req,res)=>{
+    res.send("this is my help page");
+});
+app.use((req,res)=>{
+    res.status(404).send("this page is not found");
+});
 app.listen(3000,()=>{
     console.log(`server is running at address http://localhost:3000`);
 })
